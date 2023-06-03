70d71a5cbda38d9e33cf54c646d33955ee9e6009
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2023-06-03 09:59:29 +0530

    -Added Assets

---
+++ lib/config/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  static const iconContact = 'assets/icons/contact_icon.png';
+  static const iconNotification = 'assets/icons/notification_icon.png';
+  static const iconProfile = 'assets/icons/profile_icon.png';
+  static const iconShare = 'assets/icons/share_icon.png';
+  static const iconWallet = 'assets/icons/wallet_icon.png';
+  static const iconPhone = 'assets/icons/phone_icon.png';
+  static const iconLock = 'assets/icons/lock_icon.png';
+
+  static const logoUnibit = 'assets/unibit_logo.png';
+}
